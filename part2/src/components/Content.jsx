2f91e696c196ebefebbd7068b614486f51586c7e
MarioVicente <mariovicente283@gmial.com>
2024-03-13 09:04:22 -0500

    exercise 2.5

--- part2/src/components/Content.jsx
+++ part2/src/components/Content.jsx
@@ -7,11 +7,12 @@ const Content = ({ parts }) => {
       {parts.map((part) => (
         <Part key={part.id} parts={part} />
       ))}
-      <br />
-      {`total of ${parts.reduce(
-        (sum, part) => sum + part.exercises,
-        0
-      )} exercises`}
+      <strong>
+        {`total of ${parts.reduce(
+          (sum, part) => sum + part.exercises,
+          0
+        )} exercises`}
+      </strong>
     </div>
   )
 }
